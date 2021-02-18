a17dd23a273296dd0a8bafef75767e8b5e6b6f6b
Ruslan Abdulov <abdulovr@ae.com>
2021-02-18 14:18:09 +0400

    One Direction List

---
+++ src/com/company/reverse_linked_list/ODList.java
@@ -0,0 +1,104 @@
+package com.company.reverse_linked_list;
+
+import java.util.LinkedList;
+import java.util.StringJoiner;
+
+//напишите односвязный список, напечатайте и разверните его без обратных ссылок
+class ODList<T> {
+
+    private int size = 0;
+    private Node<T> head = null;
+    private Node<T> tail = null;
+
+    public ODList() {
+        this.size = 0;
+    }
+
+    public T next() {
+        if (head == null) {
+            return null;
+        }
+
+        return head.getValue();
+    }
+
+    public Node<T> head() {
+        return head;
+    }
+
+    public ODList<T> add(T newValue) {
+        Node<T> node = new Node<>(null, newValue);
+        if (tail == null) {
+            tail = node;
+            head = tail;
+        } else {
+            tail.setNext(node);
+            tail = node;
+        }
+        size++;
+
+        return this;
+    }
+
+    public ODList<T> reverse() {
+        LinkedList<T> stack = new LinkedList<>();
+
+        Node<T> node = head;
+        while(node != null) {
+            stack.push(node.value);
+            node = node.next();
+        }
+
+        ODList<T> reverse = new ODList<>();
+        while(!stack.isEmpty()) {
+            reverse.add(stack.pop());
+        }
+        return reverse;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    static class Node<T> {
+        private Node<T> next;
+        private T value;
+
+        public Node(T value) {
+            this.value = value;
+        }
+
+        public Node(Node<T> next, T value) {
+            this.next = next;
+            this.value = value;
+        }
+
+        public Node<T> next() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public void setValue(T value) {
+            this.value = value;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner joiner = new StringJoiner(", ", ODList.class.getSimpleName() + "[", "]");
+        Node<T> node = head;
+        while(node != null) {
+            joiner.add(node.getValue().toString());
+            node = node.next();
+        }
+
+        return joiner.toString();
+    }
+}
---
+++ test/com/company/reverse_linked_list/ODListTest.java
@@ -0,0 +1,23 @@
+package com.company.reverse_linked_list;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+public class ODListTest {
+
+    private ODList<String> list = new ODList<String>().add("1").add("2").add("3");
+
+    @Test
+    public void testToString() {
+        assertEquals("ODList[1, 2, 3]", list.toString());
+    }
+
+    @Test
+    public void testReverse() {
+        assertEquals("ODList[3, 2, 1]", list.reverse().toString());
+    }
+}
